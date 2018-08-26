acf9284fd8693dde63d7e9f6f879916e150a4553
Jordan Cole Hunt <jordancolehunt@gmail.com>
2018-08-26 14:27:08 +0200

    add redux dev tools

--- src/store/quiz_app.store.js
+++ src/store/quiz_app.store.js
@@ -1,16 +1,17 @@
 import thunkMiddleware from "redux-thunk"
 import { createLogger } from "redux-logger"
-import { createStore, applyMiddleware } from "redux"
+import { createStore, applyMiddleware, compose } from "redux"
 import quizApp from "../reducers/quiz_app.reducer"
 
-const middleware = [thunkMiddleware];
+const middleware = [thunkMiddleware]
 const loggerMiddleware = createLogger()
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
-if (process.env.TEST_ENV === 'true') {
-  middleware.push(loggerMiddleware)
+if (process.env.TEST_ENV !== "true") {
+	middleware.push(loggerMiddleware)
 }
 
 export default createStore(
-  quizApp,
-  applyMiddleware(...middleware)
+	quizApp,
+	composeEnhancers(applyMiddleware(...middleware))
 )
